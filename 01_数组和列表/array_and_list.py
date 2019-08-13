429e1df9dd6136e23ff219e9d56910c84d6e022d
2285166171@qq.com <31072281+tao-to-python@users.noreply.github.com>
2019-08-13 21:40:43 +0800

    上传代码

--- 01_数组和列表/array_and_list.py
+++ 01_数组和列表/array_and_list.py
@@ -1,1 +1,37 @@
-import pandas as pd
+#-*- coding:utf-8 —*-
+
+from array import array
+
+class Array(object):
+    def __init__(self, size):
+        self._size = size
+        self._items = [None]*self._size
+        
+    def __getitem__(self,index):
+        return self._items[index]
+    
+    def __setitem__(self,index,value):
+        self._items[index] = value
+        
+    def __len__(self):
+        return self._size
+    
+    def clear(self):
+        for i in range(len(self._items)):
+            self._items[i] = None
+            
+            
+    def __iter__(self):
+        for item in self._items:
+            yield item
+            
+
+
+def test_array():
+    size = 10
+    a = Array(size)
+    a[0] = 1
+ 
+    assert a[0] == 1
+    assert len(a) == 10
+    # assert a[0] == 2
